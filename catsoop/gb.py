81c4200f917927231f79f661ff7eb4ed70e4088b
adam j hartz <hartz@mit.edu>
2016-06-27 13:33:03 -0400

    move config checks to separate file

--- catsoop/gb.py
+++ catsoop/gb.py
@@ -1,8 +1,3 @@
-# This file is part of CAT-SOOP
-# Copyright (c) 2011-2016 Adam Hartz <hartz@mit.edu>
-# CAT-SOOP is free software, licensed under the terms described in the LICENSE
-# file.  If you did not receive a copy of this file with CAT-SOOP, please see:
-# https://cat-soop.org/LICENSE
 """
 Global data
 
@@ -10,9 +5,6 @@ Many of the variables in this file are special variables that affect the way
 the page is rendered (these special variables can be overwritten by early loads
 or late loads at lower levels).
 """
-import os
-import stat
-import traceback
 
 cs_all_pieces = ['api', 'auth', 'dispatch', 'context', 'language', 'loader',
                  'logging', 'mail', 'time', 'tools', 'tutor']
@@ -44,15 +36,6 @@ Which authentication type to use (C{'login'} to use a form, C{'cert'} to read
 client certificates).
 """
 
-
-def cs_ajax_secret(username):
-    """
-    A function mapping from usernames to "secret" keys.  These should be hard
-    to guess given the username that is inputted (i.e., you should override
-    this function in C{config.py}, and ideally again within each course's
-    preload).
-    """
-    return str(hash(username))
 
 # Default Page Content
 cs_title = 'CAT-SOOP'
@@ -172,42 +155,8 @@ def cs_debug(value, tag=''):
         return
     from datetime import datetime
     with open(cs_debug_log_location, 'a') as myfile:
-        print >> myfile, datetime.now().time(), tag, value
+        print(datetime.now().time(), tag, value, file=myfile)
 
-# Checks for valid configuration
-
-_cs_config_errors = []
-
-# store original cs_ajax_secret so we can make sure it has been
-# overridden later on.
-_cs_ajax_secret_orig = cs_ajax_secret
-
-try:
-    from config import *
-except Exception as e:
-    _cs_config_errors.append('error in config.py: %s' % (e, ))
-
-# check for valid fs_root
-_fs_root_error = ('cs_fs_root must be a directory containing the '
-                  'cat-soop source code')
-if not os.path.isdir(cs_fs_root):
-    _cs_config_errors.append(_fs_root_error)
-else:
-    root = os.path.join(cs_fs_root, 'catsoop')
-    if not os.path.isdir(root):
-        _cs_config_errors.append(_fs_root_error)
-    else:
-        contents = os.listdir(root)
-        if not all(('%s.py' % i in contents or i in contents)
-                   for i in cs_all_modules):
-            _cs_config_errors.append(_fs_root_error)
-# check for valid data_root
-if not os.path.isdir(cs_data_root):
-    _cs_config_errors.append('cs_data_root must be an existing directory')
-else:
-    if not os.access(cs_data_root, os.W_OK):
-        _cs_config_errors.append('the web server must be able to write to '
-                                 'cs_data_root')
 
 try:
     https = cs_env.get('HTTPS', '0')
