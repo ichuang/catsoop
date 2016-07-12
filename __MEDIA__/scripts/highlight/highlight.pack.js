e2fff876660f819f07d35d78e1e13cb2516e5245
adam j hartz <hartz@mit.edu>
2016-07-12 15:53:35 -0400

    formatting of comments

--- __MEDIA__/scripts/highlight/highlight.pack.js
+++ __MEDIA__/scripts/highlight/highlight.pack.js
@@ -1,4 +1,23 @@
-/*! highlight.js v9.5.0 | BSD3 License | git.io/hljslicense */
+/*  This file is part of CAT-SOOP
+ *  Copyright (c) 2011-2016 Adam Hartz <hartz@mit.edu>
+ *
+ *  This program is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or (at your
+ *  option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ *  License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see
+ *  <https://www.gnu.org/licenses/agpl-3.0-standalone.html>.
+ */
+
+// Modified version of highlight.js v9.5.0, which is available under a BSD
+// 3-Clause License (see git.io/hljslicense)
 (function(factory) {
 
   // Find the global object for export to both the browser and web workers.
