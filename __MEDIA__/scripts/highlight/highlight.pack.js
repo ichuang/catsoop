d60805b2cb0651344cef71d97ac5d3b67da60f83
adam j hartz <hartz@mit.edu>
2016-07-12 15:52:19 -0400

    add default cat-soop theme, and update highlight.js python handling

--- __MEDIA__/scripts/highlight/highlight.pack.js
+++ __MEDIA__/scripts/highlight/highlight.pack.js
@@ -2694,7 +2694,25 @@ hljs.registerLanguage('python', function(hljs) {
         'exec else break not with class assert yield try while continue del or def lambda ' +
         'async await nonlocal|10 None True False',
       built_in:
-        'Ellipsis NotImplemented'
+        'ArithmeticError AssertionError AttributeError BaseException BlockingIOError ' +
+        'BrokenPipeError BufferError BytesWarning ChildProcessError ConnectionAbortedError ' +
+        'ConnectionError ConnectionRefusedError ConnectionResetError DeprecationWarning ' +
+        'EOFError Ellipsis EnvironmentError Exception False FileExistsError FileNotFoundError ' +
+        'FloatingPointError FutureWarning GeneratorExit IOError ImportError ImportWarning ' +
+        'IndentationError IndexError InterruptedError IsADirectoryError KeyError ' +
+        'KeyboardInterrupt LookupError MemoryError NameError NotADirectoryError NotImplemented ' +
+        'NotImplementedError OSError OverflowError PendingDeprecationWarning PermissionError ' +
+        'ProcessLookupError RecursionError ReferenceError ResourceWarning RuntimeError ' +
+        'RuntimeWarning StopAsyncIteration StopIteration SyntaxError SyntaxWarning SystemError ' +
+        'SystemExit TabError TimeoutError True TypeError UnboundLocalError UnicodeDecodeError ' +
+        'UnicodeEncodeError UnicodeError UnicodeTranslateError UnicodeWarning UserWarning ' +
+        'ValueError Warning ZeroDivisionError _ __build_class__ __debug__ __doc__ __import__ ' +
+        '__loader__ __name__ __package__ __spec__ abs all any ascii bin bool bytearray bytes ' +
+        'callable chr classmethod compile complex copyright credits delattr dict dir divmod ' +
+        'enumerate eval exec exit filter float format frozenset getattr globals hasattr hash ' +
+        'help hex id input int isinstance issubclass iter len license list locals map max ' +
+        'memoryview min next object oct open ord pow print property quit range repr reversed ' +
+        'round set setattr slice sorted staticmethod str sum super tuple type vars zip'
     },
     illegal: /(<\/|->|\?)/,
     contains: [
