5d7a16d0963d46aaf158813ce58c9a46ad9ceb7b
adam j hartz <hartz@mit.edu>
2016-08-13 23:13:29 -0400

    move theme processing to make it work for error pages as well

--- catsoop/dispatch.py
+++ catsoop/dispatch.py
@@ -263,6 +263,12 @@ def display_page(context):
     """
     Generate the HTTP response for a dynamically-generated page.
     """
+    if context['cs_process_theme']:
+        context['cs_theme'] = ("%s/cs_util/process_theme"
+                               "?theme=%s"
+                               "&preload=%s") % (context['cs_url_root'],
+                                                 context['cs_theme'],
+                                                 context['cs_original_path'])
     headers = {'Content-type': 'text/html'}
     if context.get('cs_user_info', {}).get('real_user', None) is not None:
         impmsg = ('<center><b><font color="red">'
@@ -436,12 +442,7 @@ def main(environment):
             return res
         elif 'cs_post_handle' in context:
             context['cs_post_handle'](context)
-        if context['cs_process_theme']:
-            context['cs_theme'] = ("%s/cs_util/process_theme"
-                                   "?theme=%s"
-                                   "&preload=%s") % (context['cs_url_root'],
-                                                     context['cs_theme'],
-                                                     context['cs_original_path'])
+
         out = display_page(context)  # tweak and display HTML
 
         session_data = context['cs_session_data']
