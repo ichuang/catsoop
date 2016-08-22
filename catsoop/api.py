132cc3faa4b481edac59cb8621cbf536d4fd4b0d
adam j hartz <hartz@mit.edu>
2016-08-21 21:21:54 -0400

    refactor get_user_information API endpoint

--- catsoop/api.py
+++ catsoop/api.py
@@ -13,6 +13,7 @@
 
 # API authentication
 
+import os
 import time
 import uuid
 import random
@@ -49,3 +50,66 @@ def get_logged_in_user(context):
             log['api_token'] = tok
             return log
     return None
+
+
+def get_user_information(context):
+    login = context['csm_auth'].get_auth_type_by_name(context, 'login')
+    
+    user = None
+    error = None
+   
+    cs_form = context['cs_form']
+    log = context['csm_cslog']
+    if 'api_token' in cs_form:
+        # if there is an API token, check it.
+        user = get_logged_in_user(context)
+        if user is None:
+            error = "Invalid API token: %s" % cs_form['api_token']
+        else:
+            extra_info = log.most_recent(None, user['username'],
+                                           'extra_info', {})
+            user.update(extra_info)
+    else:
+        if 'username' in cs_form and 'password_hash' in cs_form:
+            # if no API token was given, but username and password were, check
+            # those.
+            uname = cs_form['username']
+            pwd = cs_form['password_hash']
+            hash_iters = context.get('cs_password_hash_iterations', 250000)
+            pwd_check = login.check_password(context, pwd, uname, hash_iters)
+            if not pwd_check:
+                error = 'Invalid username or password.'
+            else:
+                user = log.most_recent(None, uname, 'logininfo', None)
+        else:
+            error = 'API token or username and password hash required.'
+    
+    if user is None and error is None:
+        # catch-all error: if we haven't authenticated but don't have an error
+        # messge, use this one.
+        error = 'Could not authenticate'
+    
+    if error is None and 'course' in cs_form:
+        # if we have successfully logged in and a course is specified, we need to
+        # look up extra information from the course in question.
+        course = cs_form['course']
+        ctx = context['csm_loader'].spoof_early_load([course])
+    
+        ctx['cs_form'] = {}
+        if 'as' in cs_form:
+            ctx['cs_form']['as'] = cs_form['as']
+    
+        base_loc = os.path.join(context['cs_data_root'], 'courses', course)
+        if os.path.isdir(base_loc):
+            uname = user['username']
+            ctx['cs_user_info'] = user
+            user = context['csm_auth']._get_user_information(ctx, user,
+                                                             course, uname,
+                                                             do_preload=True)
+        else:
+            error = 'No such course: %s' % course
+    
+    if error is not None:
+        return {'ok': False, 'error': error}
+    else:
+        return {'ok': True, 'user_info': user}
