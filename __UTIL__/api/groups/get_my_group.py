9da0d920ce0df9f939439c0c6d0af8c55b1c142e
adam j hartz <hartz@mit.edu>
2016-08-22 08:52:47 -0400

    add get_my_group API endpoint

---
+++ __UTIL__/api/groups/get_my_group.py
@@ -0,0 +1,50 @@
+# This file is part of CAT-SOOP
+# Copyright (c) 2011-2016 Adam Hartz <hartz@mit.edu>
+
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the Soopycat License, version 1.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the Soopycat License for more details.
+
+# You should have received a copy of the Soopycat License along with this
+# program.  If not, see <https://smatz.net/soopycat>.
+
+import json
+
+api_token = cs_form.get('api_token', None)
+path = cs_form.get('path', None)
+
+error = None
+
+if api_token is None:
+    error = "api_token is required"
+
+try:
+    path = opath = json.loads(path)
+    course, path = path[0], path[1:]
+except:
+    error = "invalid path: %s" % path
+
+if error is None:
+    output = csm_api.get_user_information(globals(), api_token=api_token, course=course)
+    if output['ok']:
+        uinfo = output['user_info']
+    else:
+        error = 'Could not get user information'
+
+if error is None:
+    ctx = csm_loader.spoof_early_load(opath) 
+    section, group = csm_groups.get_group(ctx, course, path, uinfo['username'])
+    if section is None and group is None:
+        error = "%s has not been assigned to a group" % uinfo['username']
+
+if error is not None:
+    output = {'ok': False, 'error': error}
+else:
+    output = {'ok': True, 'section': section, 'group': group} 
+
+cs_handler = 'raw_response'
+content_type = 'application/json'
+response = json.dumps(output)
