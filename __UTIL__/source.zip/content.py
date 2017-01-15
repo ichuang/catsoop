c51702ae8863e0e5b08b65fe7caaa4b17e83b3aa
adam j hartz <hartz@mit.edu>
2017-01-14 22:25:10 -0500

    source.zip fixes (more pedantic, and better grammar)

--- __UTIL__/source.zip/content.py
+++ __UTIL__/source.zip/content.py
@@ -12,7 +12,9 @@
 # program.  If not, see <https://smatz.net/soopycat>.
 
 import os
+import ast
 import platform
+import subprocess
 
 from zipfile import ZipFile, ZIP_DEFLATED
 
@@ -53,24 +55,53 @@ def add_files_to_zip(zipfile, base_dir, zip_base):
                 name = fullname.replace(base_dir, zip_base)
                 zipfile.write(fullname, name)
 
+def _updated_time(x):
+    if os.path.isdir(x):
+        # get mtime for dirs in a roundabout way...
+        try:
+            # build a little pipeline to recursively find the timestamp of the
+            # most recently modified file in a directory
+            p = subprocess.Popen(['find', x, '-name', '__LOGS__', '-prune', '-o', '-type', 'f', '-printf', '%T@ %P\n'], stdout=subprocess.PIPE)
+            p2 = subprocess.Popen(['sort', '-n'], stdin=p.stdout, stdout=subprocess.PIPE)
+            p3 = subprocess.Popen(['awk', '{print $1}'], stdin=p2.stdout, stdout=subprocess.PIPE)
+            p4 = subprocess.Popen(['tail', '-n', '1'], stdin=p3.stdout, stdout=subprocess.PIPE)
+            return ast.literal_eval(p4.communicate()[0].decode())
+        except:
+            return os.stat(x).st_mtime
+    return os.stat(x).st_mtime
+
 tmp = os.environ.get('TEMP',cs_fs_root) if platform.system() == 'Windows' else '/tmp'
+
+source_modified = _updated_time(cs_fs_root)
+if course is not None:
+    plugins_base = os.path.join(cs_data_root, 'courses', course, '__PLUGINS__')
+    qtypes_base = os.path.join(cs_data_root, 'courses', course, '__QTYPES__')
+    handlers_base = os.path.join(cs_data_root, 'courses', course, '__HANDLERS__')
+    authtypes_base = os.path.join(cs_data_root, 'courses', course, '__AUTH__')
+    for i in [qtypes_base, plugins_base, handlers_base, authtypes_base]:
+        try:
+            t = _updated_time(i)
+            if t > source_modified:
+                source_modified = t
+        except:
+            pass
 
 cache_fname = os.path.join(tmp, '.catsoop-source-%s.zip' % hash((cs_fs_root, course)))
 regenerate = False
 if not os.path.isfile(cache_fname):
     regenerate = True
 else:
-    source_modified = os.stat(cs_fs_root).st_mtime
-    cache_modified = os.stat(cache_fname).st_mtime
+    cache_modified = _updated_time(cache_fname)
     if source_modified > cache_modified:
         regenerate = True
+
 
 if regenerate:
     with csm_tools.filelock.FileLock(cache_fname) as flock:
         outfile = ZipFile(cache_fname, 'w', ZIP_DEFLATED)
         add_files_to_zip(outfile, cs_fs_root, 'cat-soop-src/cat-soop')
         now = csm_time.from_detailed_timestamp(cs_timestamp)
-        now = csm_time.long_timestamp(now).replace('; ', ' at')
+        now = csm_time.long_timestamp(now).replace('; ', ' at ')
         if course is None:
             outfile.writestr('cat-soop-src/README.catsoop-source',
                              SOURCE_README_NOCOURSE % (cs_url_root, now))
@@ -81,23 +112,15 @@ if regenerate:
                              SOURCE_README % (cs_url_root, now,
                                               course_name,
                                               course))
-            plugins_base = os.path.join(cs_data_root, 'courses',
-                                        course, '__PLUGINS__')
             add_files_to_zip(outfile,
                              plugins_base,
                              'cat-soop-src/%s/__PLUGINS__' % course)
-            qtypes_base = os.path.join(cs_data_root, 'courses',
-                                        course, '__QTYPES__')
             add_files_to_zip(outfile,
                              qtypes_base,
                              'cat-soop-src/%s/__QTYPES__' % course)
-            handlers_base = os.path.join(cs_data_root, 'courses',
-                                        course, '__HANDLERS__')
             add_files_to_zip(outfile,
                              handlers_base,
                              'cat-soop-src/%s/__HANDLERS__' % course)
-            authtypes_base = os.path.join(cs_data_root, 'courses',
-                                        course, '__AUTH__')
             add_files_to_zip(outfile,
                              authtypes_base,
                              'cat-soop-src/%s/__AUTH__' % course)
