94d6f39f39c317db5aa0867633bcb5e9c27275ef
adam j hartz <hartz@mit.edu>
2017-07-24 15:53:33 -0400

    change logging structure

--- catsoop/cslog.py
+++ catsoop/cslog.py
@@ -29,52 +29,48 @@ prep = lambda x: repr(x).encode()
 unprep = lambda x: ast.literal_eval(x.decode())
 
 
-def update_log(course, db_name, logname, new):
+def update_log(username, path, logname, new):
     """
     Adds a new entry to the specified log.
     """
-    course = course or '__no_course__'
     conn = r.connect(db='catsoop')
-    r.table('logs').insert({'course': course, 'username': db_name,
-                            'logname': logname, 'data': new,
-                            'time': r.now()}).run(conn)
+    r.table('logs').insert({'path': path, 'username': username, 'logname': logname, 'data': new, 'time': r.now()}).run(conn)
     conn.close()
 
 
-def overwrite_log(course, db_name, logname, new):
+def overwrite_log(username, path, logname, new):
     """
     Overwrites the most recent entry in the specified log.
     """
-    course = course or '__no_course__'
+    # this is a gross query, but... branch on whether the given log is empty.
+    # if it's empty, add a new entry
+    # otherwise, modify the existing entry
     conn = r.connect(db='catsoop')
-    r.table('logs').filter((r.row['course'] == course) &
-                           (r.row['username'] == db_name) &
-                           (r.row['logname'] == logname)).delete().run(conn)
-    r.table('logs').insert({'course': course, 'username': db_name,
-                            'logname': logname, 'data': new,
-                            'time': r.now()}).run(conn)
+    r.table('logs').get_all([username, path, logname], index='log').isEmpty().do(
+        lambda empty: r.branch(empty,
+                               r.table('logs').insert({'path': path, 'username': username, 'logname': logname, 'data': new, 'time': r.now()}),
+                               r.table('logs').get_all([username, path, logname], index='log').order_by(r.desc('time')).limit(1).update({'data': new, 'time': r.now()}))
+    ).run(conn)
     conn.close()
 
 
-def read_log(course, db_name, logname):
+def read_log(username, path, logname):
     """
     Reads all entries of a log.
     """
-    course = course or '__no_course__'
     conn = r.connect(db='catsoop')
-    res = r.table('logs').get_all([db_name, course, logname], index='log').run(conn)
+    res = r.table('logs').get_all([username, path, logname], index='log').run(conn)
     out = [i['data'] for i in res]
     conn.close()
     return out
 
 
-def most_recent(course, db_name, logname, default=None):
+def most_recent(username, path, logname, default=None):
     """
     Reads the most recent entry from a log.
     """
-    course = course or '__no_course__'
     conn = r.connect(db='catsoop')
-    res = r.table('logs').get_all([db_name, course, logname], index='log').order_by(r.desc('time')).limit(1).run(conn)
+    res = r.table('logs').get_all([username, path, logname], index='log').order_by(r.desc('time')).limit(1).run(conn)
     if len(res) == 0:
         out = default
     else:
@@ -83,15 +79,18 @@ def most_recent(course, db_name, logname, default=None):
     return out
 
 
-def modify_most_recent(course, db_name, log, default=None, transform_func=lambda x: x, method='update'):
-    course = course or '__no_course__'
+def modify_most_recent(username, path, log, default=None, transform_func=lambda x: x, method='update'):
     conn = r.connect(db='catsoop')
     if method == 'overwrite':
-        r.table('logs').get_all([db_name, course, logname], index='log').order_by(r.desc('time')).limit(1).update(lambda post: {'data': transform_func(post['data'])}).run(conn)
+        r.table('logs').get_all([username, path, logname], index='log').isEmpty().do(
+            lambda empty: r.branch(empty,
+                                   [],
+                                   r.table('logs').get_all([username, path, logname], index='log').order_by(r.desc('time')).limit(1).update(lambda post: {'data': transform_func(post['data']), 'time': r.now()}))
+        ).run(conn)
     else:
-        res = r.table('logs').get_all([db_name, course, logname], index='log').order_by(r.desc('time')).limit(1).run(conn)
+        res = r.table('logs').get_all([username, path, logname], index='log').order_by(r.desc('time')).limit(1).run(conn)
         for row in res:
-            r.table('logs').insert({'course': course, 'username': db_name,
+            r.table('logs').insert({'path': path, 'username': db_name,
                                     'logname': logname, 'data': transform_func(row['data']),
                                     'time': r.now()}).run(conn)
     conn.close()
