718c84bf9af0755a3118f401eaac7d25cc39bac1
adam j hartz <hartz@mit.edu>
2017-07-24 22:07:51 -0400

    move setup code for rethinkdb into start_catsoop.py

--- scripts/start_catsoop.py
+++ scripts/start_catsoop.py
@@ -22,6 +22,8 @@ import ctypes
 import signal
 import subprocess
 
+import rethinkdb as r
+
 os.setpgrp()
 
 scripts_dir = os.path.abspath(os.path.dirname(__file__))
@@ -33,7 +35,6 @@ if base_dir not in sys.path:
 import catsoop.base_context as base_context
 
 procs = (
-    (scripts_dir, ['rethinkdb'], 5, 'RethinkDB Server'),
     (scripts_dir, ['node', 'checker_reporter.js',
                    str(base_context.cs_websocket_server_port)], 0.1, 'Reporter'),
     (scripts_dir, ['python3', 'checker.py'], 0.1, 'Checker'),
@@ -49,6 +50,41 @@ def set_pdeathsig(sig = signal.SIGTERM):
     def callable():
         return libc.prctl(1, sig)
     return callable
+
+# Start RethinkDB First
+
+print('Starting RethinkDB Server')
+running.append(subprocess.Popen(['rethinkdb'], cwd=scripts_dir,
+                                preexec_fn=set_pdeathsig(signal.SIGTERM)))
+
+# And give it some time
+time.sleep(5)
+
+# Now make sure the database is set up
+
+c = r.connect()
+try:
+    r.db_create('catsoop').run(c)
+except:
+    pass
+c.close()
+
+c = r.connect(db='catsoop')
+
+tables = r.table_list().run(c)
+if 'logs' not in tables:
+    r.table_create('logs').run(c)
+    r.table('logs').index_create('log', [r.row['username'], r.row['path'], r.row['logname']]).run(c)
+    r.table('logs').index_wait('log').run(c)
+
+if 'checker' not in tables:
+    r.table_create('checker').run(c)
+    r.table('checker').index_create('progress').run(c)
+    r.table('checker').index_wait('progress').run(c)
+
+c.close()
+
+# Finally, start the workers.
 
 for (wd, cmd, slp, name) in procs:
     print('Starting', name)
