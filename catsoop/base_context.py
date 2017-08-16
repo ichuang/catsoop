0cd651ea3047e734884b1a1556a5979f53112f96
adam j hartz <hartz@mit.edu>
2017-08-16 15:18:40 -0400

    admin for next developments

--- catsoop/base_context.py
+++ catsoop/base_context.py
@@ -21,7 +21,7 @@ the page is rendered (these special variables can be overwritten by early loads
 or late loads at lower levels).
 """
 
-cs_version = 'v10.2.0'
+cs_version = '(development version)'
 """
 CAT-SOOP's version number
 """
