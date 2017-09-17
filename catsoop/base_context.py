ff3f3f766d10fe75d48a5ff9bcb560e4f7bfb0b7
adam j hartz <hartz@mit.edu>
2017-09-17 17:07:51 -0400

    release version 11.0.3

--- catsoop/base_context.py
+++ catsoop/base_context.py
@@ -21,7 +21,7 @@ the page is rendered (these special variables can be overwritten by early loads
 or late loads at lower levels).
 """
 
-cs_version = 'v11.0.2'
+cs_version = 'v11.0.3'
 """
 CAT-SOOP's version number
 """
