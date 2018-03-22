b00f5a3062a3dbae195dddb90233709fea0dbdad
adam j hartz <hz@mit.edu>
2018-03-22 16:01:37 -0400

    update ply to v3.11

--- catsoop/tools/ply/__init__.py
+++ catsoop/tools/ply/__init__.py
@@ -1,13 +1,5 @@
 # PLY package
 # Author: David Beazley (dave@dabeaz.com)
-"""
-Python Lex-Yacc (Parser Generator)
 
-From <http://www.dabeaz.com/ply/>
-"""
-
-__version__ = '3.9'
+__version__ = '3.11'
 __all__ = ['lex','yacc']
-
-from . import yacc
-from . import lex
