17eab003b722afd3e92b2e8e94cafadccd632d4e
ichuang <ichuang@mit.edu>
2018-12-30 23:17:21 -0500

    simplify logging format

--- catsoop/debug_log.py
+++ catsoop/debug_log.py
@@ -5,12 +5,7 @@ import logging
 def setup_logging(context):
     logging.getLogger('pylti.common').setLevel(context.get('cs_lti_debug_level', 'WARNING'))
     logging.getLogger("cs").setLevel(context.get('cs_debug_level', 'WARNING'))
-
-    ch = logging.StreamHandler()	# console handler for logging
-    ch.setLevel(context.get('cs_debug_level', 'WARNING'))
-    formatter = logging.Formatter('%(asctime)s - %(message)s')
-    ch.setFormatter(formatter)
-    logging.getLogger("cs").addHandler(ch)
+    logging.basicConfig(format='%(asctime)s - %(message)s')
 
 LOGGER = logging.getLogger("cs")
 
