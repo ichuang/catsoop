63fcfbc043fd3d5cba82d914fcab8560cf5971aa
ichuang <ichuang@mit.edu>
2018-12-30 23:17:24 -0500

    rearrange directories to make catsoop pip installable

--- catsoop/lti.py
+++ catsoop/lti.py
@@ -7,12 +7,15 @@ LTI Tool Provider interface
 import os
 import uuid
 import string
+import urllib
 import random
 import hashlib
 import pylti.common
 
 from lxml import etree
 from lxml.builder import ElementMaker
+
+from oauthlib.oauth1 import Client
 
 from . import auth
 from . import session
@@ -165,6 +168,80 @@ class lti4cs_response(object):
                 for _from, _to in mapping.items():
                     url = url.replace(_from, _to)
         return url
+
+#-----------------------------------------------------------------------------
+
+class LTI_Consumer(object):
+    '''
+    Simple LTI tool consumer (useful for unit-tests of CAT-SOOP acting as an LTI tool provider)
+    '''
+    def __init__(self, lti_url="", username="lti_user", service_url="http://localhost",
+                 consumer_key="consumer_key", secret="secret_key"):
+        self.lti_url = lti_url
+        self.username = username
+        self.service_url = service_url
+        self.consumer_key = consumer_key
+        self.lti_context = self._get_lti_context(self.lti_url, secret=secret)
+
+    def _get_lti_context(self, lti_url, secret=None):
+        """
+        Generate the LTI context for a specific LTI url request
+
+        lti_url: (str) URL to the LTI tool provider
+        secret: (str) shared secret with the LTI tool provder
+        """
+        body = {
+            'tool_consumer_instance_guid': u'lti_test_%s' % self.lti_url,
+            'user_id': self.username + "__LTI__1234",
+            'roles': u'[student]',
+            'context_id': u"catsoop_test",
+            'lti_url': self.lti_url,
+            'lti_version': u'LTI-1p0',
+            'lis_result_sourcedid': self.username,
+            'lis_outcome_service_url': self.service_url,
+            'lti_message_type': 'basic-lti-launch-request',	# required, see https://www.imsglobal.org/specs/ltiv2p0/implementation-guide#toc-21
+            'resource_link_id': '123',
+        }
+        retdat = body.copy()
+        key = self.consumer_key
+        self._sign_lti_message(body, key, secret, lti_url)
+        LOGGER.info("[unit_tests] signing OAUTH with key=%s, secret=%s, url=%s" % (key, secret, lti_url))
+        retdat.update(dict(
+            lti_url=lti_url,
+            oauth_consumer_key=key,
+            oauth_timestamp=body['oauth_timestamp'],
+            oauth_nonce=body['oauth_nonce'],
+            # oauth_signature=urllib.parse.unquote(body['oauth_signature']).encode('utf8'),
+            oauth_signature=urllib.parse.unquote(body['oauth_signature']),
+            oauth_signature_method=body['oauth_signature_method'],
+            oauth_version=body['oauth_version'],
+        ))
+        return retdat
+    
+    def _sign_lti_message(self, body, key, secret, url):
+        client = Client(
+            client_key=key,
+            client_secret=secret
+        )
+    
+        __, headers, __ = client.sign(
+            url,
+            http_method=u'POST',
+            body=body,
+            headers={'Content-Type': 'application/x-www-form-urlencoded'}
+        )
+    
+        auth_header = headers['Authorization'][len('OAuth '):]
+        auth = dict([param.strip().replace('"', '').split('=') for param in
+                     auth_header.split(',')])
+    
+        body['oauth_nonce'] = auth['oauth_nonce']
+        body['oauth_signature'] = auth['oauth_signature']
+        body['oauth_timestamp'] = auth['oauth_timestamp']
+        body['oauth_signature_method'] = auth['oauth_signature_method']
+        body['oauth_version'] = auth['oauth_version']
+    
+#-----------------------------------------------------------------------------
 
 def serve_lti(context, path_info, environment, params, dispatch_main):
     '''
