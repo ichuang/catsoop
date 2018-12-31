f29356ccaff85ed65d32c96c0f2c99294cac8564
adam j hartz <hz@mit.edu>
2018-12-30 23:20:21 -0500

    update setup.py information

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ requirements = open("requirements.txt").read().split('\n')
 
 setup(
     name='catsoop',
-    version='0.9',
-    author='catsoop-users@mit.edu',
-    author_email='catsoop-users@mit.edu',
+    version='13.0.0+',
+    author='CAT-SOOP Contributors',
+    author_email='catsoop-dev@mit.edu',
     packages=['catsoop', 'catsoop.test'],
     scripts=[],
-    url='http://pypi.python.org/pypi/catsoop/',
-    license='LICENSE.txt',
-    description='CAT-SOOP is an Automatic Tutor for Six-Oh-One Problems',
-    long_description=open('README.md').read(),
+    url='https://catsoop.mit.edu',
+    license='GNU AGPLv3+',
+    description='CAT-SOOP is a tool for automatic collection and assessment of online exercises.',
+    long_description=open('README').read(),
     include_package_data=True,
     entry_points={
         'console_scripts': [
@@ -23,6 +23,15 @@ setup(
     install_requires=requirements,
     package_dir={'catsoop': 'catsoop'},
     package_data={'catsoop': ['scripts/*']},
-    # data_files = data_files,
     test_suite="catsoop.test",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Education",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+    ]
 )
