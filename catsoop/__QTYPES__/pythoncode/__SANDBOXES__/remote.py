e9cb811dc3f8dcf4ba9d5bf93372d90c343ed0aa
adam j hartz <hz@mit.edu>
2019-01-19 17:24:11 -0500

    change remote sandbox location to a new spot for new catsoop

--- catsoop/__QTYPES__/pythoncode/__SANDBOXES__/remote.py
+++ catsoop/__QTYPES__/pythoncode/__SANDBOXES__/remote.py
@@ -17,7 +17,7 @@
 import json
 import urllib.request, urllib.parse, urllib.error
 
-SANDBOX_URL = "https://cat-soop.org/python3_sandbox/"
+SANDBOX_URL = "https://cat-soop.org/python3_sandbox_14/"
 
 
 def run_code(context, code, options, count_opcodes=False, opcode_limit=None):
