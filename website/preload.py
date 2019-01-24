a90a5e1673d71cd7aba4f316cdaa37bace52d1ba
adam j hartz <hz@mit.edu>
2019-01-24 14:50:28 -0500

    code formatting

--- website/preload.py
+++ website/preload.py
@@ -1,5 +1,5 @@
 cs_auth_required = False
-cs_long_name = 'CAT-SOOP Website'
+cs_long_name = "CAT-SOOP Website"
 
 
 cs_footer = """<a href="http://creativecommons.org/licenses/by-sa/4.0/" rel="license"><img alt="Creative Commons License" src="COURSE/cc-by-sa.png" style="border-width:0"></a><br/>The contents of this page are Copyright &copy; 2016-2019 by the CAT-SOOP Developers.<br/>They are licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.<br/>The original form of this web site is source code in the CAT-SOOP specification
