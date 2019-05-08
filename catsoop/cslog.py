3a437da709dd5722c297e6405995f9b50acc72b0
adam j hartz <hz@mit.edu>
2019-05-08 14:12:06 -0400

    docstring

--- catsoop/cslog.py
+++ catsoop/cslog.py
@@ -21,10 +21,13 @@ From a high-level perspective, CAT-SOOP's logs are sequences of Python objects.
 A log is identified by a `db_name` (typically a username), a `path` (a list of
 strings starting with a course name), and a `logname` (a string).
 
-On disk, each log is a file containing pretty-printed Python objects separated
-by blank lines.  This is an implementation detail that most people shouldn't
-need to worry about, but it does mean that log files cna be read or manipulated
-manually, in addition to using the functions in this module.
+On disk, each log is a file containing one or more entries, where each entry
+consists of:
+
+* 8 bits representing the length of the entry
+* a binary blob (pickled Python object, potentially encrypted and/or
+    compressed)
+* the 8-bit length repeated
 
 This module provides functions for interacting with and modifying those logs.
 In particular, it provides ways to retrieve the Python objects in a log, or to
