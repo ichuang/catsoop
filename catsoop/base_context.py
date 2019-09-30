66011a5d7a03e900d9e618e466369ea6f97f90cb
ichuang <ichuang@mit.edu>
2019-09-30 08:03:28 -0400

    base_context.py import debugging

--- catsoop/base_context.py
+++ catsoop/base_context.py
@@ -426,6 +426,7 @@ for i in cs_all_pieces:
             exec("csm_%s = %s" % (i, i))
         except Exception as err:
             LOGGER.error("[base_context] failed in import assignment of csm_%s to %s" % (i, i))
+            LOGGER.error("[base_context] cs_all_pieces=%s" % cs_all_pieces)
             LOGGER.error("traceback=%s" % traceback.format_exc())
             raise
 
