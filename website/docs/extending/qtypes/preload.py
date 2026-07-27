1ef1b8ff7b240996538f98d5d64b4e8a56eb15cf
ichuang <ichuang@mit.edu>
2026-07-27 15:46:14 -0400

    docs on custom questions; self-partner as case study

---
+++ website/docs/extending/qtypes/preload.py
@@ -0,0 +1,3 @@
+cs_order = 0
+cs_long_name = "Designing Custom Question Types"
+cs_title = "Custom Question Types | %s" % (cs_title,)
---
+++ website/docs/extending/self_partner/preload.py
@@ -0,0 +1,3 @@
+cs_order = 0.1
+cs_long_name = "Case Study: self_partner"
+cs_title = "self_partner Case Study | %s" % (cs_title,)
