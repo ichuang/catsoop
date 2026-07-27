6e3c64f0468d4786abce4bc5fee88d6e2602fefd
ichuang <ichuang@mit.edu>
2026-07-27 15:27:05 -0400

    docs for static pages and dynamic python-generated content

---
+++ website/docs/authoring/dynamic_pages/preload.py
@@ -0,0 +1,3 @@
+cs_order = 0.4
+cs_long_name = "Dynamic Python-Generated Pages"
+cs_title = "Dynamic Python Pages | %s" % (cs_title,)
---
+++ website/docs/authoring/static_content/preload.py
@@ -0,0 +1,3 @@
+cs_order = 0.3
+cs_long_name = "Images and Other Static Content"
+cs_title = "Static Content | %s" % (cs_title,)
